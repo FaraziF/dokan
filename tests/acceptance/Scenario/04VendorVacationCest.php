e475e171808d1319ad5a07d55c01f49fca57fe1c
Shanjidah Afroz <wedevs@android-e953b0e8c3feff6b.localdomain>
2020-02-11 09:54:06 +0600

    Vendor Vacation

--- tests/acceptance/Scenario/04VendorVacationCest.php
+++ tests/acceptance/Scenario/04VendorVacationCest.php
@@ -13,29 +13,29 @@ class VendorVacationCest
       {
         $I->loginAsVendor();
         $I->click('Settings');
-        $I->checkOption('#dokan-seller-vacation-activate');
-        $I->selectOption('settings_closing_style','datewise');
+        if ($I->tryToSeeCheckboxIsChecked('#dokan-seller-vacation-activate'))
+            {
+                $I->checkOption('#dokan-seller-vacation-activate');
+            }
+        $I->wait(8);
         $I->click('//*[@id="dokan-seller-vacation-date-from"]');
-        $I->pressKey('//*[@id="dokan-seller-vacation-date-from"]','2019-11-22');
+        $I->fillField('//*[@id="dokan-seller-vacation-date-from"]','2020-02-06');
         $I->wait(3);
         $I->click('//*[@id="dokan-seller-vacation-date-to"]');
-        $I->pressKey('//*[@id="dokan-seller-vacation-date-to"]','2019-11-22');
+        $I->fillField('//*[@id="dokan-seller-vacation-date-to"]','2020-11-22');
         $I->wait(4);
-        $I->fillField('dokan_seller_vacation_datewise_message','vacation 2');
-       // $I->waitForElement('#dokan-seller-vacation-save-edit',3);
-       // $I->click('//*[@id="dokan-seller-vacation-save-edit"]');
-        //$I->click(//button[contains(.,“Save”)]’);
+        $I->fillField('dokan_seller_vacation_datewise_message','Eid Vacation');
         $I->click('//button[contains(.,"Save")]');
         $I->wait(3);
         $I->click('Update Settings');
-        $I->wait(10);
       }
 
      public function customerviewVendorVacation(\Step\Acceptance\MultiSteps $I)
         {
           $I->loginAsCustomer();
-          $I->click('Store List');
-          $I->click('//div[@id="dokan-seller-listing-wrap"]/div/ul/li/div/div[2]/a');
+          $I->wait(3);
+          $I->amonpage('/store-listing/');
+          $I->click(['css'=>'.dokan-single-seller:nth-child(1) .dashicons']);
           $I->scrollTo('#primary', 100,600);
           $I->wait(5);
         }
